47830cb926e5c6effbf2745b1379937515ff89c7
Joshua Goins <josh@redstrate.com>
2025-03-02 17:39:41 -0500

    Add more documentation for DateTime

--- src/structure/datetime.rs
+++ src/structure/datetime.rs
@@ -2,10 +2,15 @@ use binrw::binrw;
 
 use crate::property::PropertyBase;
 
+/// A date and time.
+///
+/// See [the Unreal Engine documentation](https://dev.epicgames.com/documentation/en-us/unreal-engine/API/Runtime/Core/Misc/FDateTime?application_version=4.27).
 #[binrw]
 #[derive(Debug)]
 pub struct DateTimeStruct {
-    pub unk: [u8; 8],
+    /// Number of ticks.
+    /// The ticks are 0.1 microseconds (= 100 nanoseconds) since January 1, 0001.
+    pub ticks: i64,
 }
 
 impl PropertyBase for DateTimeStruct {
